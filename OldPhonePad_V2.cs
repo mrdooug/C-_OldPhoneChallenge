0c5578ce9d4da211bc7bcb7bd1995ef06d5aea62
mrdooug <51678360+mrdooug@users.noreply.github.com>
2023-06-16 09:33:45 -0300

    Add files via upload

---
+++ OldPhonePad_V2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public class OldPhone
+{
+    public static string OldPhonePad(string input)
+    {
+        // Define the mapping of button numbers to letters
+        string[] buttonMapping = new string[]
+        {
+            " ",    // Button 0
+            "",     // Button 1
+            "ABC",  // Button 2
+            "DEF",  // Button 3
+            "GHI",  // Button 4
+            "JKL",  // Button 5
+            "MNO",  // Button 6
+            "PQRS", // Button 7
+            "TUV",  // Button 8
+            "WXYZ"  // Button 9
+        };
+
+        string output = "";
+        string[] inputs = input.Split(' ');
+
+        foreach (string inputPart in inputs)
+        {
+            if (string.IsNullOrEmpty(inputPart))
+            {
+                output += " ";
+            }
+            else
+            {
+                int buttonNumber = int.Parse(inputPart[0].ToString());
+                if (buttonNumber >= 0 && buttonNumber < buttonMapping.Length)
+                {
+                    int pressCount = inputPart.Length;
+                    int index = (pressCount - 1) % buttonMapping[buttonNumber].Length;
+                    output += buttonMapping[buttonNumber][index];
+                }
+            }
+        }
+
+        return output;
+    }
+
+    public static void Main()
+    {
+        bool tryAgain = true;
+        while (tryAgain)
+        {
+            Console.Write("Enter input: ");
+            string input = Console.ReadLine();
+
+            string output = OldPhonePad(input);
+            Console.WriteLine("Output: " + output);
+
+            Console.Write("Do you want to try again? (Y/N): ");
+            string choice = Console.ReadLine();
+            tryAgain = (choice.ToUpper() == "Y");
+        }
+    }
+}
---
+++ OldPhone_V1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+public class OldPhonePadConverter
+{
+    private static readonly Dictionary<char, string> keypadMapping = new Dictionary<char, string>
+    {
+        { '1', " " },
+        { '2', "ABC" },
+        { '3', "DEF" },
+        { '4', "GHI" },
+        { '5', "JKL" },
+        { '6', "MNO" },
+        { '7', "PQRS" },
+        { '8', "TUV" },
+        { '9', "WXYZ" },
+        { '0', "0" }
+    };
+
+    public static string ConvertInput(string input)
+    {
+        string output = string.Empty;
+        List<char> previousChars = new List<char>();
+        int pauseDuration = 2000; // Pause duration in milliseconds
+
+        foreach (char currentChar in input)
+        {
+            if (currentChar == '#' || currentChar == '*')
+            {
+                // Exit key encountered, end of input
+                break;
+            }
+            else if (currentChar == ' ')
+            {
+                // Pause between characters on the same button
+                if (previousChars.Count > 0)
+                {
+                    Thread.Sleep(pauseDuration);
+                }
+            }
+            else if (keypadMapping.ContainsKey(currentChar))
+            {
+                // Valid keypad character found
+                string keypadLetters = keypadMapping[currentChar];
+                int letterIndex = previousChars.Count % keypadLetters.Length;
+                char convertedChar = keypadLetters[letterIndex];
+                output += convertedChar;
+                previousChars.Add(currentChar);
+            }
+            else
+            {
+                // Invalid character, ignore
+            }
+        }
+
+        return output;
+    }
+
+    public static void Main()
+    {
+        string input;
+        string output;
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.WriteLine("Enter the input numbers:");
+            input = Console.ReadLine();
+            output = OldPhonePadConverter.ConvertInput(input + "#");
+            Console.WriteLine("Output: " + output);
+
+            Console.WriteLine("Press 1 to try again, press 2 to exit.");
+            char option = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            switch (option)
+            {
+                case '1':
+                    Console.WriteLine();
+                    break;
+                case '2':
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Exiting...");
+                    exit = true;
+                    break;
+            }
+        }
+    }
+}
